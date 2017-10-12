aad4bd82d43a80fd7497bf62e593b55f8dbb637c
Madalin Tache <madalin@madalin.eu>
2017-10-12 21:05:31 +0300

    Icons changed, fixed broken functionality. Added options.

--- background.js
+++ background.js
@@ -1,38 +1,74 @@
-var isEnabled = true;
-var maxTabs = 30;
-var tabsCount;
+// Icons from <div>Icons made by <a href="https://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 
-function updateBadgeText() {
-    chrome.browserAction.setBadgeText({
-        text: "" + tabsCount + "/" + maxTabs
-    });
+let isEnabled = true;
+var maxTabs;
+var tabsCount;
+let tabLimit;
+let limit;
+
+
+chrome.storage.sync.get("tabsLimit", function(data) {
+    maxTabs = data.tabsLimit;
+    // console.log(data);
+    console.log(maxTabs);
+    // var settingEnabled = data.someSetting;
+});
+
+// Notification data
+var opt = {
+    type: "basic",
+    title: "Oh noes!",
+    message: "You hit your tabs limit!",
+    iconUrl: "icons/tab_128.png",
 }
 
+// Updates the badge text and background color.
+function updateBadgeText() {
+    percent = tabsCount / maxTabs * 100;
+    // console.log(percent);
+    if (percent >= 25 && percent < 50) {
+        chrome.browserAction.setBadgeBackgroundColor({ color: '#13B618' })
+    } else if (percent >= 50 && percent < 75) {
+        chrome.browserAction.setBadgeBackgroundColor({ color: '#E77714' })
+    } else if (percent >= 75 && percent <= 100) {
+        chrome.browserAction.setBadgeBackgroundColor({ color: '#CD482C' })
+    }
+    chrome.browserAction.setBadgeText({ text: "" + tabsCount });
+}
+
+// Updates the tab count.
 function updateTabsCount() {
     chrome.tabs.query({
         windowType: 'normal',
         pinned: false
-    }, function (tabs) {
+    }, function(tabs) {
         tabsCount = tabs.length;
         updateBadgeText();
     });
 }
 
+// Handles a new tab and closes it if the limit has been hit.
 function handleTabCreated(tab) {
+    tabid = tab.id;
+    console.log('count:' + tabsCount);
+    console.log('max:' + maxTabs);
     if (tabsCount >= maxTabs) {
-        chrome.tabs.remove(tab.id);
-    }
-    else {
+        // console.log(tab);
+        chrome.tabs.remove(tabid);
+        chrome.notifications.create(opt);
+    } else {
         updateTabsCount();
     }
 }
 
+// Handles the tab removed
 function handleTabRemoved(tab) {
     updateTabsCount();
 }
 
+// This handles when the tab has been updated. Unused for now.
 function handleTabUpdated(tab) {
-    updateTabsCount();
+    // updateTabsCount();
 }
 
 function init() {
@@ -48,18 +84,30 @@ function teardown() {
     chrome.tabs.onUpdated.removeListener(handleTabUpdated);
 }
 
-chrome.browserAction.onClicked.addListener(function (tab) {
-    if (!isEnabled) {
-        init();
-        chrome.browserAction.setIcon({ path: "icons/19.png" });
-    }
-    else {
-        teardown();
-        chrome.browserAction.setIcon({ path: "icons/19-disabled.png" });
-        chrome.browserAction.setBadgeText({ 'text': '' });
-    }
-
-    isEnabled = !isEnabled;
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.create({ 'url': chrome.extension.getURL('options.html') }, function(tab) {
+        // Launching options :)
+    });
 });
+
+// chrome.browserAction.onClicked.addListener(function (tab) {
+//     if (!isEnabled) {
+//         init();
+//         chrome.tabs.create({'url': chrome.extension.getURL('options.html')}, function(tab) {
+//           // Tab opened.
+//         });
+//         // chrome.browserAction.setIcon({ path: "icons/tab_128.png" });
+//     }
+//     else {
+//         teardown();
+//         chrome.tabs.create({'url': chrome.extension.getURL('options.html')}, function(tab) {
+//           // Tab opened.
+//         });
+//         // chrome.browserAction.setIcon({ path: "icons/tab_24.png" });
+//         // chrome.browserAction.setBadgeText({ 'text': '' });
+//     }
+
+//     isEnabled = !isEnabled;
+// });
 
 init();
---
+++ js/options.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let maxTabs; // Default
+let tabsLimit;
+
+
+// Saves options to chrome.storage.sync.
+function save_options() {
+  var limit = document.getElementById('limit').value || 20;
+  console.log('Limita ' + limit);
+  chrome.storage.sync.set({
+    tabsLimit: limit
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+    // Let's reload the extension.
+    chrome.runtime.reload();
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+    chrome.storage.sync.get("tabsLimit", function (data) {
+        maxTabs = data.tabsLimit || 20;
+        document.getElementById('limit').value = maxTabs;
+        // console.log(data);
+        // var settingEnabled = data.someSetting;
+    });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
